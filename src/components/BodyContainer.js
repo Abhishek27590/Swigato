c34c6d51cb18b6900dff933314a92928517fd0fa
Abhishek27590 <10abhishekpanda@gmail.com>
2024-11-17 16:25:13 +0530

    Added swiggy's API and added Shimmer Effect

--- src/components/BodyContainer.js
+++ src/components/BodyContainer.js
@@ -1,15 +1,30 @@
-import { useState } from "react";
-import data from "../utils/data"
+import { useEffect, useState } from "react";
 import ResCards from "./ResCards";
+import Shimmer from "./Shimmer";
 
 const BodyContainer=()=>{
-    const [list,updateList]=useState(data)
+    const [list,updateList]=useState([])
+
+    useEffect(()=>{
+        getData()
+    },[])
+
+    const getData=async()=>{
+        const res=await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65200&lng=77.16630&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+        const jsonRes=await res.json()
+        updateList(jsonRes?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+        
+    }
+
+    if(list.length===0){
+        return <Shimmer/>
+    }
     return(
         <div>
             <div>
                 <button className="filter-btn" onClick={()=>
                 {
-                    const filterlist=list.filter((temp)=>temp.info.avgRating>4)
+                    const filterlist=list.filter((temp)=>temp.info.avgRating>4.3)
                     console.log(filterlist)
                     updateList(filterlist)
                 }
